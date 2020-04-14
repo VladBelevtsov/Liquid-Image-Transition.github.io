f8768bc24598bd482aa6e0c4b0ffa7d28bc2ee6a
Vlad Bielievtsov <vladbelevtsovdaime@gmail.com>
2020-04-14 15:40:32 +0300

    This is my first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity: 0.2,
+    image1: "./bw1.jpg",
+    image2: "./bw2.jpg",
+    angle: Math.PI / 6,
+    displacementImage: "./diss.png",
+})
+
